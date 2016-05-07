ce374701ad179b46da9a6d23bdaed3e6546404d8
Donovan Cotter <donovan.cotter@yahoo.com>
2016-05-07 16:12:59 -0400

    first commit

---
+++ BlogIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogIt
+//
+//  Created by Donovan Cotter on 5/7/16.
+//  Copyright © 2016 DonovanCotter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
